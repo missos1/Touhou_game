b0513f8ef378915c4ca98fbdadb48af671284345
HoangMinhDuc <hrhdv2asdf@gmail.com>
2025-02-27 12:04:59 +0700

    sync

--- SDLGame1/src/Bullets.cpp
+++ SDLGame1/src/Bullets.cpp
@@ -1,24 +1,24 @@
-//#include "headers/Player.hpp"
-//#include "headers/Bullets.hpp"
-//#include <iostream>
-//
-//Bullets::Bullets(SDL_Renderer* renderer, int x, int y, int velx, int vely)
-//	: renderer(renderer), vx(velx), vy(vely) {
-//	playerbullet_text = TextureManager::LoadTexture("res/player/Reimu_sprite", renderer);
-//	srcRect = { 128, 0, 32, 32 }; // sprite size
-//	destRect = { x, y, 32 * 2, 32 * 2 }; // display size
-//}
-//
-//Bullets::~Bullets() {
-//	SDL_DestroyTexture(playerbullet_text);
-//	
-//}
-//
-//void Bullets::update() {
-//	destRect.x += vx;
-//	destRect.y += vy;
-//}
-//
-//void Bullets::render() {
-//	SDL_RenderCopy(renderer, playerbullet_text, &srcRect, &destRect);
-//}
+#include "headers/Player.hpp"
+#include "headers/Bullets.hpp"
+#include <iostream>
+
+Bullets::Bullets(SDL_Renderer* renderer, int x, int y, int velx, int vely)
+	: renderer(renderer), vx(velx), vy(vely) {
+	playerbullet_text = TextureManager::LoadTexture("res/player/Reimu_sprite", renderer);
+	srcRect = { 128, 0, 32, 32 }; // sprite size
+	destRect = { x, y, 32 * 2, 32 * 2 }; // display size
+}
+
+Bullets::~Bullets() {
+	SDL_DestroyTexture(playerbullet_text);
+	
+}
+
+void Bullets::update() {
+	destRect.x += vx;
+	destRect.y += vy;
+}
+
+void Bullets::render() {
+	SDL_RenderCopy(renderer, playerbullet_text, &srcRect, &destRect);
+}
--- SDLGame1/src/headers/Bullets.hpp
+++ SDLGame1/src/headers/Bullets.hpp
@@ -1,29 +1,29 @@
-//#ifndef BULLETS_HPP
-//#define BULLETS_HPP
-//
-//#include <SDL.h>
-//#include <SDL_image.h>
-//#include "Player.hpp"
-//#include "Game.hpp"
-//#include "sidebar.hpp"
-//#include "TextureManager.hpp"
-//
-//class Bullets {
-//public:
-//	Bullets(SDL_Renderer* renderer, int x, int y, int velx, int vely);
-//	~Bullets();
-//	void update();
-//	void render();
-//
-//private:
-//	SDL_Renderer* renderer;
-//	SDL_Texture* playerbullet_text;
-//	SDL_Rect srcRect, destRect;
-//
-//	int vx, vy;
-//	int savedVx, savedVy;
-//	SDL_Rect srcRect;
-//	SDL_Rect destRect;
-//};
-//
-//#endif
+#ifndef BULLETS_HPP
+#define BULLETS_HPP
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include "Player.hpp"
+#include "Game.hpp"
+#include "sidebar.hpp"
+#include "TextureManager.hpp"
+
+class Bullets {
+public:
+	Bullets(SDL_Renderer* renderer, int x, int y, int velx, int vely);
+	~Bullets();
+	void update();
+	void render();
+
+private:
+	SDL_Renderer* renderer;
+	SDL_Texture* playerbullet_text;
+	SDL_Rect srcRect, destRect;
+
+	int vx, vy;
+	int savedVx, savedVy;
+	SDL_Rect srcRect;
+	SDL_Rect destRect;
+};
+
+#endif
